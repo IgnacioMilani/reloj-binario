a6bede020716069d70aac750a0701add1804939e
NachoX150 <32331378+NachoX150@users.noreply.github.com>
2017-09-27 16:07:29 +0200

    Correción de tabulados

--- programa1_0.c
+++ programa1_0.c
@@ -1,12 +1,11 @@
-#include <stdio.h>		// Standard input/output definitions
+#include <stdio.h>	// Standard input/output definitions
 #include <stdlib.h>
-#include <unistd.h>		// UNIX standard function definitions
-#include <errno.h>		// Error number definitions
-#include <fcntl.h>		// File control definitions 
+#include <unistd.h>	// UNIX standard function definitions
+#include <errno.h>	// Error number definitions
+#include <fcntl.h>	// File control definitions 
 #include <termios.h>	// POSIX terminal control definitions 
-#include <string.h> 	// para memset
-#include <time.h>
-#include <stdint.h>		//Para mapa de bits
+#include <time.h>	// Para la hora
+#include <stdint.h>	// Para mapa de bits
 
 //////Definicion de funciones///////
 int iniciar_puerto(char **);
@@ -15,25 +14,25 @@ int serialport_writebyte(int, uint8_t);
 ////////////////////////////////////
 
 int main(int argc,char *argv[]){
-		int fd = -1;
-		int rc, i;
-		if( fd!=-1 )   			//Si el puerto queda abierto, lo cierra
-                close(fd);
-		fd = iniciar_puerto(argv);
-		if( fd==-1 ) perror("No se pudo abrir el puerto");
-		serialport_flush(fd);  	//Para limpiar buffer
-		
-		time_t tiempo = time(0);
-		struct tm *tlocal = localtime(&tiempo);
+	int fd = -1;
+	int rc, i;
+	if( fd!=-1 )   			//Si el puerto queda abierto, lo cierra
+	close(fd);
+	fd = iniciar_puerto(argv);
+	if( fd==-1 ) perror("No se pudo abrir el puerto");
+	serialport_flush(fd);  	//Para limpiar buffer
 
-		int sec=tlocal->tm_sec;
-		for(i=0;i<=15;i++){				//Prueba de 15 segundos
-			rc= serialport_writebyte(fd, (uint8_t)sec);		//Envia el numero
-			if(rc==-1) perror("Error de escritura");		//Variable de chequeo
-			sleep(1);
-			if (sec<59)	sec++;			//Si sec=58 pasa a sec=59
-			else sec=0;					//Si sec=59 pasa a sec=0
-		}
+	time_t tiempo = time(0);
+	struct tm *tlocal = localtime(&tiempo);
+
+	int sec=tlocal->tm_sec;
+	for(i=0;i<=15;i++){				//Prueba de 15 segundos
+		rc= serialport_writebyte(fd, (uint8_t)sec);		//Envia el numero
+		if(rc==-1) perror("Error de escritura");		//Variable de chequeo
+		sleep(1);
+		if (sec<59)  sec++;			//Si sec=58 pasa a sec=59
+		else sec=0;				//Si sec=59 pasa a sec=0
+	}
 		
         close(fd);
 }
@@ -41,8 +40,8 @@ int main(int argc,char *argv[]){
 
 int iniciar_puerto(char *argv[]){
 	struct termios toptions;
-    int tty_fd;
-    tty_fd = open(argv[1], O_RDWR | O_NONBLOCK );
+    	int tty_fd;
+    	tty_fd = open(argv[1], O_RDWR | O_NONBLOCK );
 
 	if (tty_fd == -1){
     	perror("No se pudo abrir el puerto");
@@ -51,43 +50,41 @@ int iniciar_puerto(char *argv[]){
 
 	// Configuramos velocidad a 9600 bauds
 	cfsetispeed(&toptions, B9600);
-    cfsetospeed(&toptions, B9600);
+	cfsetospeed(&toptions, B9600);
 
 	// 8N1
-    toptions.c_cflag &= ~PARENB;
-    toptions.c_cflag &= ~CSTOPB;
-    toptions.c_cflag &= ~CSIZE;
-    toptions.c_cflag |= CS8;
-    // Sin flow control
-    toptions.c_cflag &= ~CRTSCTS;
-	
+	toptions.c_cflag &= ~PARENB;
+	toptions.c_cflag &= ~CSTOPB;
+	toptions.c_cflag &= ~CSIZE;
+	toptions.c_cflag |= CS8;
+	// Sin flow control
+	toptions.c_cflag &= ~CRTSCTS;
+
 	toptions.c_cflag |= CREAD | CLOCAL;  // turn on READ & ignore ctrl lines
-    toptions.c_iflag &= ~(IXON | IXOFF | IXANY); // turn off s/w flow ctrl
+	toptions.c_iflag &= ~(IXON | IXOFF | IXANY); // turn off s/w flow ctrl
 
-    toptions.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG); // make raw
-    toptions.c_oflag &= ~OPOST; // make raw
+	toptions.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG); // make raw
+	toptions.c_oflag &= ~OPOST; // make raw
 
-    toptions.c_cc[VMIN]  = 0;
-    toptions.c_cc[VTIME] = 0;
+	toptions.c_cc[VMIN]  = 0;
+	toptions.c_cc[VTIME] = 0;
 
 	tcsetattr(tty_fd, TCSANOW, &toptions);
-    if( tcsetattr(tty_fd, TCSAFLUSH, &toptions) < 0) {
-        perror("No se pudieron configurar los atributos termios");
-        return -1;
-    }
+	if( tcsetattr(tty_fd, TCSAFLUSH, &toptions) < 0) {
+		perror("No se pudieron configurar los atributos termios");
+		return -1;
+	}
 
-    return tty_fd;
+    	return tty_fd;
 }
 
 
-int serialport_flush(int tty_fd)
-{
+int serialport_flush(int tty_fd){
     sleep(2); 		//Requerido para que funcione el flush
     return tcflush(tty_fd, TCIOFLUSH);
 }
 
-int serialport_writebyte( int tty_fd, uint8_t b)
-{
+int serialport_writebyte( int tty_fd, uint8_t b){
     int n = write(tty_fd,&b,1);
     if( n!=1)					//Variable de chequeo
         return -1;
